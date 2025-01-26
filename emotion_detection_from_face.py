1c96e34dedced24f28bb17dfc60af2c1c7e704a5
shivamutyala325 <155558495+shivamutyala325@users.noreply.github.com>
2025-01-26 18:14:32 +0530

    Add files via upload

---
+++ emotion_detection_from_face.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+from keras.models import load_model
+
+classifier=cv2.CascadeClassifier(r'C:\Users\shiva\PycharmProjects\ML_Standard\venv\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml')
+video=cv2.VideoCapture(0)
+
+model=load_model(r'conv_base.h5')
+exp=['angry', 'disgusted', 'fearful', 'happy', 'neutral', 'sad', 'surprised']
+
+while True:
+    res,frame=video.read()
+    if res:
+        #frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        faces=classifier.detectMultiScale(frame,scaleFactor=1.1,minNeighbors=5)
+        for (a,b,c,d) in faces:
+            req_face=frame[b:b+d,a:a+c]
+            req_face=cv2.resize(req_face,(48,48))
+            #cv2.imshow('req_face',req_face)
+            req_face=np.expand_dims(req_face,axis=0)
+
+
+            #print(req_face.shape)
+            #cv2.waitKey(0)
+            y=model.predict(req_face)
+            y=np.argmax(y)
+            print(exp[y])
+            cv2.rectangle(frame,(a,b),(a+c,b+d),(255,0,0),2)
+            cv2.putText(frame,f'given expression is {exp[y]}',(a-50,b),cv2.FONT_HERSHEY_SIMPLEX,.5,(255,0,0),2)
+            cv2.imshow('frames',frame)
+
+    else:
+        break
+
+    if cv2.waitKey(1)&0xFF==ord('q'):
+       break
+
+
+
+
---
+++ emtoion_detection_train.py
@@ -0,0 +1,52 @@
+from keras.utils import image_dataset_from_directory
+import matplotlib.pyplot as plt
+train_path=r'C:\Users\shiva\Downloads\emtion_from_face\train'
+test_path=r'C:\Users\shiva\Downloads\emtion_from_face\test'
+
+train_data=image_dataset_from_directory(train_path,batch_size=32,image_size=(48,48),label_mode='categorical')
+test_data=image_dataset_from_directory(test_path,batch_size=32,image_size=(48,48),label_mode='categorical')
+
+import keras
+from keras.layers import Flatten,Dense,Dropout
+from keras.applications.vgg16 import VGG16
+pre_model=VGG16(weights='imagenet',include_top=False,input_shape=(48,48,3))
+
+for layer in pre_model.layers:
+    layer.trainable=False
+x=pre_model.output
+x=Flatten()(x)
+x=Dense(units=128,activation='relu')(x)
+x=Dropout(0.1)(x)
+final_layer=Dense(units=7,activation='softmax')(x)
+
+model=keras.Model(inputs=pre_model.input,outputs=final_layer)
+model.summary()
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+history=model.fit(train_data,epochs=30,validation_data=test_data)
+
+model.save('conv_base.h5')
+loss,accuracy=model.evaluate(test_data)
+
+print(f'loss={loss} ,accuracy={accuracy}')
+
+plt.figure(figsize=(12, 6))
+
+plt.subplot(1, 2, 1)
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(loc='upper left')
+
+# Plot training & validation loss values
+plt.subplot(1, 2, 2)
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(loc='upper left')
+
+plt.tight_layout()
+plt.show()
